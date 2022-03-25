fde1df657a44f1c2b2d02e1a3b514efe3ff15bb5
JustTrott <69697147+JustTrott@users.noreply.github.com>
2022-03-25 20:39:24 +0600

    added bulk schedule parsing

--- sheet_parser.py
+++ sheet_parser.py
@@ -15,9 +15,9 @@ class SpreadsheetParser():
         weekdays_row = ws[3]
         for cell in weekdays_row:
             if cell.value is not None and str(cell.value) == weekday:
-                schedule = f"Расписание для класса {grade} на день: {str(cell.value)}\n"
                 col_bounds = cell.column_letter, weekdays_row[cell.column + 10].column_letter
                 break
+        schedule = f"Расписание для класса {grade} на день: {weekday}\n"
         for row in ws:
             if str(row[0].value).startswith(grade):
                 row_bounds = (row[0].row, row[0].row+3)
@@ -70,6 +70,74 @@ class SpreadsheetParser():
             schedule += tabulate(copy_schedule_items, headers='firstrow', tablefmt='pretty') + '\n'
         schedule += '\n' + '\n\n'.join([f'Профили{index+1}: {subject}' for index, subject in enumerate(profile_subjects)])
         return schedule
+
+    def get_grade_schedule_bulk(self, grades: list[str], weekday):
+        sheets: str = []
+        if weekday == 'auto':
+            weekday = self.get_current_weekday()
+        self.wb = load_workbook(filename=self.book_name, read_only=True)
+        ws = self.wb.active
+        weekdays_row = ws[3]
+        for cell in weekdays_row:
+            if cell.value is not None and str(cell.value) == weekday:
+                col_bounds = cell.column_letter, weekdays_row[cell.column + 10].column_letter
+                break
+        for grade in grades:
+            schedule = f"Расписание для класса {grade} на день: {weekday}\n"
+            for row in ws:
+                if str(row[0].value).startswith(grade):
+                    row_bounds = (row[0].row, row[0].row+3)
+            schedule_items = []
+            profile_subjects = []
+            for row in ws[f"{col_bounds[0]}{row_bounds[0]}" : f"{col_bounds[1]}{row_bounds[1]}"]:
+                data_cols = []
+                for cell in row:
+                    if len(str(cell.value)) > 60:
+                        target = f'{cell.value}'
+                        if target not in profile_subjects:
+                            profile_subjects.append(target)
+                            data_cols.append(f'Профили{len(profile_subjects)}')
+                        else:
+                            data_cols.append(None)
+                        continue
+                    data_cols.append(cell.value)
+                schedule_items.append(data_cols)
+
+            times_row = ws[f"{col_bounds[0]}4" : f"{col_bounds[1]}4"][0]
+            times = []
+            for cell in times_row:
+                times.append(cell.value)
+            schedule_items.insert(0, times)
+            for i in reversed(range(len(schedule_items[1]) - 1)):
+                if schedule_items[1][i] == schedule_items[1][i+1]:
+                    time = schedule_items[0][i]
+                    time = time[:time.find('-')] + schedule_items[0][i+1][schedule_items[0][i+1].find('-'):]
+                    schedule_items[0][i] = "p" + time
+                    for j in range(len(schedule_items)):
+                        del schedule_items[j][i+1]
+            for i in reversed(range(len(data_cols))):
+                for j in range(1, len(schedule_items)):
+                    if schedule_items[j][i] is not None:
+                        break
+                else:
+                    for j in range(len(schedule_items)):
+                        del schedule_items[j][i]
+            subjects_cnt = len(schedule_items[0])
+            for i in range(subjects_cnt):
+                copy_schedule_items = []
+                for j in range(len(schedule_items)):
+                    if schedule_items[j][i] is None:
+                        continue
+                    copy_schedule_items.append([schedule_items[j][i]])
+                if copy_schedule_items[0][0].startswith('p'):
+                    copy_schedule_items[0][0] = f'{i+1} Пара ' + copy_schedule_items[0][0][1:]
+                else:
+                    copy_schedule_items[0][0] = f'{i+1} Урок ' + copy_schedule_items[0][0]
+                schedule += tabulate(copy_schedule_items, headers='firstrow', tablefmt='pretty') + '\n'
+            schedule += '\n' + '\n\n'.join([f'Профили{index+1}: {subject}' for index, subject in enumerate(profile_subjects)])
+            sheets.append(schedule)
+        return sheets
+
 
     def get_current_weekday(self) -> str:
         weekdays = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"]
