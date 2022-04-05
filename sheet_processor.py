92b2ffaa89f375418584e557e05821c5bad213fe
JustTrott <69697147+JustTrott@users.noreply.github.com>
2022-04-05 22:48:14 +0600

    added a function to add a time row manually to the speadsheet

--- sheet_processor.py
+++ sheet_processor.py
@@ -1,5 +1,8 @@
-from openpyxl import load_workbook
+from openpyxl import Workbook, load_workbook
+from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.cell.cell import Cell
 from openpyxl.utils.cell import range_boundaries
+import openpyxl
 
 from config import Config
 
@@ -32,7 +35,28 @@ class SpreadsheetProcessor():
             for row in ws.iter_rows(min_col=min_col, min_row=min_row, max_col=max_col, max_row=max_row):
                 for cell in row:
                     cell.value = top_left_cell_value
+        self.add_times_row(ws)
         wb.save(self.book_name)
+    
+    def add_times_row(self, ws : Worksheet):
+        lessons_row = 4
+        lesson_time : dict[str, tuple[str, str]] = {
+            1 : ("8:15", "8:55"),
+            2 : ("9:15", "9:55"),
+            3 : ("10:00", "10:40"),
+            4 : ("10:45", "11:25"),
+            5 : ("11:30", "12:10"),
+            6 : ("12:30", "13:10"),
+            7 : ("13:30", "14:10"),
+            8 : ("14:30", "15:10"),
+            9 : ("15:25", "16:05"),
+            10 : ("16:20", "17:00")
+        }
+        ws.insert_rows(lessons_row)
+        cell : Cell
+        for cell in ws[lessons_row + 1]:
+            if cell.value in lesson_time:
+                ws[cell.coordinate].offset(-1).value = f"{lesson_time[cell.value][0]}-{lesson_time[cell.value][1]}"
     
 if __name__ == '__main__':
     sp = SpreadsheetProcessor('schedule.xlsx')
